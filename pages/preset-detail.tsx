cf41e47d8ea16df17eee6f7aec702e770b381dab
Yongwoo Kim <71542970+dmstjdhdh@users.noreply.github.com>
2025-09-16 10:03:02 +0900

    fix: FlatList 간격조정

--- pages/preset-detail.tsx
+++ pages/preset-detail.tsx
@@ -362,46 +362,50 @@ function PresetDetail() {
     scrollRef.current?.scrollToIndex({ index: Number(e), animated: false });
     setTabValue(e);
   };
-  const renderItem = ({ item, index }) => (
-      <Stack.Vertical
-          style={{
-            position: "relative",
-            borderWidth: 1,
-            borderColor: "#eeeeee",
-            borderRadius: 13,
-            paddingHorizontal: 24,
-            paddingVertical: 20,
-            marginTop: 10,
-            marginHorizontal: 24,
-          }}
-          onLayout={e => handleItemLayout(e, index)}
-      >
-        <Text typography="t5" fontWeight="medium" color={colors.blue700}>
-          {moment(day[index]).format("YY-MM-DD") + " "}({WEEKDAYS[moment(day[index]).days()]})
-        </Text>
-        <View style={{ height: 20 }} />
-        {item?.map((value, idx) => (
-            <StepperRow
-                key={idx}
-                hideLine={idx === item.length - 1}
-                left={<StepperRow.NumberIcon number={idx + 1} />}
-                center={
-                  <StepperRow.Texts
-                      type="A"
-                      title={value.name + " "}
-                      description={
-                        !value.name.includes("추천")
-                            ? Math.floor(value.takenTime / 60) !== 0
-                                ? `${Math.floor(value.takenTime / 60)}시간${value.takenTime % 60 !== 0 ? `${value.takenTime % 60}분` : ""}`
-                                : ""
-                            : ""
-                      }
-                  />
-                }
-            />
-        ))}
-      </Stack.Vertical>
-  );
+  const renderItem = ({ item, index }) => {
+    const isLast = index === presetDatas[params?.index].length - 1;
+    return (
+        <Stack.Vertical
+            style={{
+              position: "relative",
+              borderWidth: 1,
+              borderColor: "#eeeeee",
+              borderRadius: 13,
+              paddingHorizontal: 24,
+              paddingVertical: 20,
+              marginTop: 10,
+              marginBottom: isLast ? 155 : 20,
+              marginHorizontal: 24,
+            }}
+            onLayout={e => handleItemLayout(e, index)}
+        >
+          <Text typography="t5" fontWeight="medium" color={colors.blue700}>
+            {moment(day[index]).format("YY-MM-DD") + " "}({WEEKDAYS[moment(day[index]).days()]})
+          </Text>
+          <View style={{height: 20}}/>
+          {item?.map((value, idx) => (
+              <StepperRow
+                  key={idx}
+                  hideLine={idx === item.length - 1}
+                  left={<StepperRow.NumberIcon number={idx + 1}/>}
+                  center={
+                    <StepperRow.Texts
+                        type="A"
+                        title={value.name + " "}
+                        description={
+                          !value.name.includes("추천")
+                              ? Math.floor(value.takenTime / 60) !== 0
+                                  ? `${Math.floor(value.takenTime / 60)}시간${value.takenTime % 60 !== 0 ? `${value.takenTime % 60}분` : ""}`
+                                  : ""
+                              : ""
+                        }
+                    />
+                  }
+              />
+          ))}
+        </Stack.Vertical>
+    );
+  };
 
   return (
       <View style={{ flex: 1 }}>
