cef3e8ef9dccb0baff207f60ad36e6bc61aa9558
Yongwoo Kim <71542970+dmstjdhdh@users.noreply.github.com>
2025-09-18 14:19:57 +0900

    fix: 메인화면 리팩토링

---
+++ pages/main.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {Dimensions, TouchableOpacity, View, Image, StyleSheet} from 'react-native';
+import MainHome from './main/main-home';
+import MainTrip from './main/main-trip';
+import MainTravelShop from './main/main-travel-shop';
+import MainInfo from './main/main-info';
+import CustomBottomBar from '../components/main/custom-bottom-bar';
+
+export default function Main() {
+    const [tab, setTab] = useState(0);
+
+    const renderScreen = () => {
+        switch (tab) {
+            case 0:
+                return <MainHome />;
+            case 1:
+                return <MainTrip />;
+            case 2:
+                return <MainTravelShop />;
+            case 3:
+                return <MainInfo />;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            {renderScreen()}
+            <CustomBottomBar currentIndex={tab} onTabPress={setTab} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, backgroundColor: '#f8f8f8' },
+});
